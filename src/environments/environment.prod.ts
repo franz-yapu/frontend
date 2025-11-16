353caee52722e870e513ca5d5ef630e30ae1bdae
franztutator <franz.yapu@tutatordev.com>
2025-11-16 18:51:32 -0400

    Fix url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,7 +3,7 @@ import { environmentDefault } from './default';
 export const environment = {
   ...environmentDefault,
   production: true,
-  backend: 'https://vertexweb.lat/api/',  
+  backend: 'https://vertexweb.lat/api',  
   Socket: 'https://vertexweb.lat',      
   jwtKey: 'jwtToken'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,6 @@ import { environmentDefault } from "./default";
 export const environment = {
     ...environmentDefault,
     production: false,
-    backend: 'https://vertexweb.lat/api/', // Asegúrate que coincida con tu basePath en Nest
+    backend: 'https://vertexweb.lat/api', // Asegúrate que coincida con tu basePath en Nest
     jwtKey: 'jwtToken' // Key para localStorage
   };
